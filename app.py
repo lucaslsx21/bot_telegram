50537602e88bd4a06e91f2597648fee6f138cb3e
lucaslsx21 <lucaslsilva@live.com>
2024-01-29 19:14:10 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import telebot
+import dotenv
+from os import getenv
+
+
+dotenv.load_dotenv()
+
+TOKEN_TELEGRAM = getenv("TOKEN_TELEGRAM")
+
+bot = telebot.TeleBot(TOKEN_TELEGRAM)
+
+comandos = (
+    "/opcao1 - Essa é a primeira opção.",
+    "/opcao2 - Essa é a segunda opção."
+)
+
+
+@bot.message_handler(commands=["start"])
+def mensagem_boas_vindas(message):
+    bot.send_message(
+        message.chat.id, "Olá, tudo bem? Escolha uma das seguintes opções:")
+    bot.send_message(message.chat.id, comandos[0])
+    bot.send_message(message.chat.id, comandos[1])
+
+# mostrar um determinado menu
+
+
+@bot.message_handler(commands=["opcao1"])
+def opcao1(message):
+    bot.send_message(message.chat.id, "Você escolheu a primeira opção.")
+
+
+@bot.message_handler(commands=["opcao2"])
+def opcao2(message):
+    bot.send_message(message.chat.id, "Você escolheu a segunda opção.")
+
+# Resposta para o usuario
+
+
+@bot.message_handler(func=lambda message: "Bom dia" in message.text)
+def responde_usuario(message):
+    nome_usuario = message.from_user.first_name
+    resposta = f"Olá, {nome_usuario}. Em que posso ajudar?"
+    bot.reply_to(message, resposta)
+
+
+if __name__ == "__main__":
+    bot.polling()
+
+# t.me/Basquete_29012024_bot
